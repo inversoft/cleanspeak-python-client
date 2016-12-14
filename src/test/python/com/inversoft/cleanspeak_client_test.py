d1ccd6c27486eb0eb8e9d585ca4336407ba43163
Brian Pontarelli <brian@inversoft.com>
2016-12-14 16:03:53 -0700

    Removed Passport code

--- src/test/python/com/inversoft/cleanspeak_client_test.py
+++ src/test/python/com/inversoft/cleanspeak_client_test.py
@@ -74,53 +74,6 @@ class ClientTest(unittest2.TestCase):
 
         self.assertEqual(cr.status, 200)
 
-    #
-    # def print_json(self, parsed_json):
-    #     print json.dumps(parsed_json, indent=2, sort_keys=True)
-    #
-    # def test_retrieve_applications(self):
-    #     client_response = self.client.retrieve_applications()
-    #     self.assertEqual(client_response.status, 200)
-    #     self.assertEqual(len(client_response.success_response['applications']), 3)
-    #
-    # def test_create_user_retrieve_user(self):
-    #     # Check if the user already exists.
-    #     get_user_response = self.client.retrieve_user_by_email('art@vandaleyindustries.com')
-    #     if get_user_response.status is 200:
-    #         delete_user_response = self.client.delete_user(get_user_response.success_response['user']['id'])
-    #         self.assertEqual(delete_user_response.status, 200, delete_user_response.error_response)
-    #     else:
-    #         self.assertEqual(get_user_response.status, 404, get_user_response.error_response)
-    #
-    #     # Create a new registration for this user.
-    #     user_request = {
-    #         'sendSetPasswordEmail': False,
-    #         'skipVerification': True,
-    #         'user': {
-    #             'email': 'art@vandaleyindustries.com',
-    #             'password': 'password'
-    #         }
-    #     }
-    #     create_user_response = self.client.create_user(None, user_request)
-    #     self.assertEqual(create_user_response.status, 200, create_user_response.error_response)
-    #
-    #     # Retrieve the user
-    #     user_id = create_user_response.success_response['user']['id']
-    #     get_user_response = self.client.retrieve_user(user_id)
-    #     self.assertEqual(get_user_response.status, 200)
-    #     self.assertIsNotNone(get_user_response.success_response)
-    #     self.assertIsNone(get_user_response.error_response)
-    #     self.assertEquals(get_user_response.success_response['user']['email'], 'art@vandaleyindustries.com')
-    #     self.assertFalse('password' in get_user_response.success_response['user'])
-    #     self.assertFalse('salt' in get_user_response.success_response['user'])
-    #
-    # def test_retrieve_user_missing(self):
-    #     user_id = uuid.uuid4()
-    #     client_response = self.client.retrieve_user(user_id)
-    #     self.assertEqual(client_response.status, 404)
-    #     self.assertEqual(client_response.success_response, None)
-    #     self.assertIsNotNone(client_response.error_response)
-
 
 if __name__ == '__main__':
     unittest2.main()
